1fe14669e158d52b8e3ce4d94536e948dbbd6cd9
Артем Крамар <artem.kramar@knu.ua>
2026-04-08 14:23:49 +0300

    Add file reading logic

--- text_analyzer.py
+++ text_analyzer.py
@@ -12,3 +12,8 @@ def count_words(text: str) -> int:
     text_cleaned = re.sub(r'\.\.\.|[.!?]', ' ', text)
     words = re.split(r'[,\s:;]+', text_cleaned)
     return len([w for w in words if w.strip()])
+
+def analyze_file(filepath: str) -> tuple:
+    with open(filepath, 'r', encoding='utf-8') as f:
+        content = f.read()
+    return count_words(content), count_sentences(content)
