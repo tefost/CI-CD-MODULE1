7a665ffd80b037f356e8a72f63fcb62d7b9c5ae2
Артем Крамар <artem.kramar@knu.ua>
2026-04-08 14:20:07 +0300

    Add function to count words

--- text_analyzer.py
+++ text_analyzer.py
@@ -5,3 +5,10 @@ def count_sentences(text: str) -> int:
         return 0
     sentences = re.split(r'\.\.\.|[.!?]', text)
     return len([s for s in sentences if s.strip()])
+
+def count_words(text: str) -> int:
+    if not text.strip():
+        return 0
+    text_cleaned = re.sub(r'\.\.\.|[.!?]', ' ', text)
+    words = re.split(r'[,\s:;]+', text_cleaned)
+    return len([w for w in words if w.strip()])
